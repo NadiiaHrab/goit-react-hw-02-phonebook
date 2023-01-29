c1dc422485b7d7c6651f5e7b9b7f143137bf1f3e
DemouserNG <108471967+NadiiaHrab@users.noreply.github.com>
2023-01-29 13:59:36 +0200

    add nanoid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
-import Contactform from "./Contactform";
+import Form from "./Form";
+import React, { Component } from 'react';
 
-export const App = () => {
-  return (
-    <Contactform contacts={[]} name={''} />
-  );
+class App extends Component  {
+
+ state = {
+    contacts: [],
+
+  }
+  formSubmitHandler = data => {
+console.log(data);
+  }
+   
+  render() {
+    return (
+      <Form onSubmit={this.formSubmitHandler} />
+) 
+
+}
+
 };
+
+export default App;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
-class Contactfrom extends Component {
+
+
+class Form extends Component {
   state = {
-    contacts: [],
     name: '',
     number: ''
   
   }
+
+  nanoidIdName = nanoid();
+  nanoidIdNumber = nanoid();
   
   handleChange = event=> {
     // console.log(event.currentTarget);
@@ -17,40 +22,51 @@ class Contactfrom extends Component {
       [event.currentTarget.name]: event.currentTarget.value,
     })
   }
-  
-  handleSubmit = event => {
+
+   handleSubmit = event => {
     event.preventDefault();
-    console.log(this.state);
+    //  console.log(this.state);
+     this.props.onSubmit(this.state)
+
+     this.reset();
   }
 
+  reset = () => {
+    this.setState({name: '', number: ''})
+  }
+  
   render() {
 
     return ( 
    <>
-  <h2 className=''>Phonebook</h2>
+    <h2 className=''>Phonebook</h2>
+        
 <form onSubmit={this.handleSubmit}>
-  <label htmlFor=''>Name
+  <label htmlFor={this.nanoidIdName}>Name
         <input
           type="text"
           name="name"
           value={this.state.name}
           onChange={this.handleChange}
+          id={this.nanoidIdName}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           />
-          </label>
+    </label>      
           
-          <label htmlFor=''>Number
+    <label htmlFor={this.nanoidIdNumber}>Number
           <input
             type="tel"
             name="number"
             value={this.state.number}
             onChange={this.handleChange}
+            id={this.nanoidIdNumber}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required />
-          </label>
+            required
+          />
+    </label>    
         <button type='submit'>Add contact</button>
       
  </form> 
@@ -59,4 +75,4 @@ class Contactfrom extends Component {
 
 }
 }
-export default Contactfrom;
+export default Form;
